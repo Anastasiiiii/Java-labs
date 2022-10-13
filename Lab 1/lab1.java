44cdbb761623cc5d6b5a3c13990d905442b791f7
Anastasia Fartushnyak <74658637+Anastasiiiii@users.noreply.github.com>
2022-10-13 16:18:22 +0300

    Update lab1.java

--- Lab 1/lab1.java
+++ Lab 1/lab1.java
@@ -2,25 +2,12 @@ import java.util.Scanner;
 
 public class sumCount {
     public static void main(String[] args) {
-        int m, n;
-        int b, a;
+        int m = 2, n = 3;
+        double b = 1.2, a = 2.3;
         int S = 0;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter m: ");
-        m = in.nextInt();
-        System.out.println(m);
-        System.out.print("Enter b: ");
-        b = in.nextInt();
-        System.out.println(b);
-        System.out.print("Enter n: ");
-        n = in.nextInt();
-        System.out.println(n);
-        System.out.print("Enter a: ");
-        a = in.nextInt();
-        System.out.println(a);
         if (b <= 0){
             System.out.println("Error!");
-        } else if (m < b || n < a) {
+        } else if (m <= b || n <= a) {
             System.out.println("Error");
         } else
             for (double j = b; j <= m; j++) {
