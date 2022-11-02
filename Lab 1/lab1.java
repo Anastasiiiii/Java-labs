33b25d76701a7c506fc5749793ad49d5e1064e1d
Anastasia Fartushnyak <74658637+Anastasiiiii@users.noreply.github.com>
2022-11-02 11:26:20 +0200

    Update lab1.java
    Add scunner and additional terms

--- Lab 1/lab1.java
+++ Lab 1/lab1.java
@@ -6,14 +6,41 @@ public class sumCount {
     //C3 = 0, тому С = 0.
     //C5 = 1, тому О2 = /.
     //C7 = 6, тому тип індексів i та j double.
+    
+   
+    
     public static void main(String[] args) {
-        int m = 2, n = 3;
-        double b = 1.2, a = 2.3;
+        int m, n;
+        double a;
+        double b;
         int S = 0;
-        if (b <= 0){
+        
+         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        String aDouble = in.nextLine();
+        a = Double.parseDouble(aDouble);
+        System.out.println(a);
+
+        System.out.print("Enter b: ");
+        String bDouble = in.nextLine();
+        b = Double.parseDouble(bDouble);
+        System.out.println(b);
+
+        System.out.print("Enter m: ");
+        m = in.nextInt();
+        System.out.println(m);
+
+        System.out.print("Enter n: ");
+        n = in.nextInt();
+        System.out.println(n);
+        
+        if (b == 0){
+            System.out.println("Error!");
+        } else if (a < 0 || b < 0 || m < 0 || n < 0) {
             System.out.println("Error!");
         } else if (m <= b || n <= a) {
-            System.out.println("Error");
+            System.out.println("Error!");
         } else
             for (double j = b; j <= m; j++) {
                 for (double i = a; i <= n; i++) {
