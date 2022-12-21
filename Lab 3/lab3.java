13afd817a40972ecbaa4a8a726a7b577e85fe2d4
Anastasia Fartushnyak <74658637+Anastasiiiii@users.noreply.github.com>
2022-12-21 14:35:41 +0200

    Update lab3.java
    Sort array with wordsCounters

--- Lab 3/lab3.java
+++ Lab 3/lab3.java
@@ -26,7 +26,11 @@ public class lab3 {
                 System.out.println();
 
             }
-
+             
+            Arrays.sort(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
 
         }
     }
