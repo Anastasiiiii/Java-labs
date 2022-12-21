8fc15a48aee80166f8379f7aa4177a642b8aec3d
Anastasia Fartushnyak <74658637+Anastasiiiii@users.noreply.github.com>
2022-12-21 14:34:38 +0200

    Create arrays
    Add word counters to array. Word counters - amount of words in the sentences.

--- Lab 3/lab3.java
+++ Lab 3/lab3.java
@@ -4,18 +4,25 @@ public class lab3 {
 
             StringBuilder text = new StringBuilder(
                            " Нарцис вузьколистий росте в Україні лише на Закарпатті. Долина нарцисів – унікальна природна пам’ятка в урочищі Креші поблизу міста Хуст. Це єдине місце в Східній Європі, де існують природні зарості цієї білосніжної квітки. Схожі, але менші за площею популяції є ще в таких місцях: Альпах, Румунії, деяких країнах на Балканах. Науковці вважають, що багато тисячоліть тому, рятуючись від льодовика, квіти переселилися в хустську долину. Про це місце люди переповідають багато легенд. Одна з них є переспівом давньогрецького міфу про Нарциса");
+            
+                
             System.out.println(text + "\n");
             String subStr [] = text.toString().split("[.!?]");
             
+            int arr[] = new int[subStr.length];
+            int arr2[] = new int[subStr.length];    
+            
             for (int i = 0; i <= subStr.length - 1; i++) {
                 System.out.println("Слова, що були у речені:");
-                int t = 0;
+                int wordsCounter = 0;
                 String w[] = subStr[i].toString().trim().split(" ");
                 for (int j = 0; j <= w.length - 1; j++) {
-                    t++;
+                    wordsCounter++;
                     System.out.println(w[j]);
                 }
-                System.out.println("Кількість слів у даному речені: " + t);
+                arr[i] = wordsCounter;
+                arr2[i] = wordsCounter;
+                System.out.println("Кількість слів у даному речені: " + wordsCounter);
                 System.out.println();
 
             }
